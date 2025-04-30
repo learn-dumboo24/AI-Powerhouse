c7ef4a42abac7a98da1c68e0e2765d28a44a1ade
learn-dumboo24 <sanketjha160@gmail.com>
2025-04-30 22:23:30 +0530

    50% frontend

--- src/assets/navbar.jsx
+++ src/assets/navbar.jsx
@@ -3,15 +3,16 @@ function Navbar(){
     function Reload(){
         return(window.location.reload());
     }
-    function Waves(){
-        return (b+a);
-    }
+    
     return(
         <>
         <div className="navbar">
             <div className ="navbar-container">
-                <button onClick ={Reload}>Home</button>
-                <button>Get Started</button>
+                <button onClick ={Reload} className="text-white text-3xl">Home</button>
+                <div className="flex w-[140px] h-[45px] bg-gradient-to-r from-purple-700 via-violet-950 to-purple-900 rounded-lg text-gray-300 text-lg font-semibold shadow-lg transform transition-all duration-300 hover:scale-105 justify-center">
+                    <button className="text-2xl">Get Started</button>
+                </div>
+                
             </div>
         </div>
         </>
